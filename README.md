4d2658508ad302da67a5ecc9c5aaca265f9c5e5f
romulocreso <101442676+romulocreso@users.noreply.github.com>
2022-03-12 02:21:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculadora-funcional
+ Esse projeto foi realizado segundo requisitado pelo Programa Desenvolve 2022 do Grupo Boticário. Abordando a criação de uma calculadora funcional utilizando HTML, CSS e JavaScript.
---
+++ script.js
@@ -0,0 +1,73 @@
+var btn 	  = document.querySelectorAll(".key li"),
+	input 	  = document.querySelector(".input-valor"),
+	operador  = document.querySelectorAll(".operador");
+
+for(var i = 0; i < btn.length; i++){
+	document.onkeypress = function(event){
+		var key = event.keyCode; 
+		for(var e = 0; e <= 10; e++){
+				if(key === (48+e)){
+					input.innerHTML += e;
+				}
+			}
+			switch (key){
+				case 42:
+					input.innerHTML += "*";
+					break;
+				case 43:
+					input.innerHTML += "+";
+					break;
+				case 45:
+					input.innerHTML += "-";
+					break;
+				case 46:
+					input.innerHTML += ".";
+					break;
+				case 47:
+					input.innerHTML += "/";
+					break;
+				case 13:
+				case 61:
+					var equacao = input.innerHTML;
+					if(equacao){
+						try {
+							input.innerHTML = eval(equacao);
+						} catch (e) {
+							alert('Erro na expressão');
+						} 
+					}
+					break;
+				case 67:
+				case 99:
+						input.innerHTML = "";
+						break;						
+					default:
+						break;
+		}
+	};
+	btn[i].addEventListener('click',function(){
+		var btnVal 	 = this.innerHTML,
+			inputVal = input.innerHTML;
+		switch (btnVal){
+			case "AC":
+				input.innerHTML = "";
+				break;
+			case "=":
+				var equacao = inputVal;
+
+			if(equacao){
+				try {
+					input.innerHTML = eval(equacao);
+					} catch (e) {
+						alert('Erro na expressão');
+
+					} 
+				}
+				break;
+				default:
+					input.innerHTML += btnVal;
+					break;	
+			}
+
+		});
+	}
